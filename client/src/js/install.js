c4f7f1e716101da51cf4464d97efa5d85d978c20
P Luong <p-luong@live.co.uk>
2022-11-12 18:36:08 +0000

    imported required classes links and scripts

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,6 +13,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    
     const promptEvent = window.deferredPrompt;
 
     if (!promptEvent){
