90c08d10ac2835c1fcc72555862a06fd51a50bf7
P Luong <p-luong@live.co.uk>
2022-11-12 16:50:05 +0000

    added logic for indexedDB put operation

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,13 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  console.error('Update database');
+export const putDb = async (id, content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('Data updated to database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
