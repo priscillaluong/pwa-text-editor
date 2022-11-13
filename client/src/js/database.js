1e50598df205030d5d249adf5764bf5a92d8da15
P Luong <p-luong@live.co.uk>
2022-11-13 13:46:24 +0000

    removed comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
@@ -22,7 +22,7 @@ export const putDb = async (id, content) => {
   console.log('Data updated to database', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
+// Gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
